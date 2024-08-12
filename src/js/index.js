a329de5d3e8a42e6c6fef11d1530cb193dc50117
Dariet-SP <159501959+Dariet-SP@users.noreply.github.com>
2024-08-12 18:13:17 +0000

    alas

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,31 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Counter from "./component/counter";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let counter = 0
 
+setInterval(() => {
+    const sixth = Math.floor(counter / 100000) % 10;
+    const fifth = Math.floor(counter / 10000) % 10;
+    const fourth = Math.floor(counter / 1000) % 10;
+    const third = Math.floor(counter / 100) % 10;
+    const second = Math.floor(counter / 10) % 10;
+    const first = Math.floor(counter / 1) % 10;
+    counter++
+    
+    ReactDOM.render(
+        <div>
+            <Counter firstNumber={first} secondNumber={second} thirdNumber={third} fourthNumber={fourth} fifthNumber={fifth} sixthNumber={sixth}/>
+        </div>
+    
+    , document.querySelector("#app"));
+
+}, 1000);
